67adc2ef483b01b79e7476128697feabbb2e6b9c
vivalenta <vivalenta@ukr.net>
2018-11-07 09:48:16 +0200

    Update webui.lua

--- webui.lua
+++ webui.lua
@@ -6,7 +6,7 @@ local dec64 = require("mime").decode("base64")
 local msg_prefix = "[webui] "
 
 local options = {
-  port = 8081,
+  port = 8080,
   disable = false,
   logging = false,
   ipv4 = true,
